d51d2e25b68e7e460f80e29375ad78f1f112469f
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-08-28 23:00:59 +0400

    Create Parkour.java

---
+++ src/main/java/de/vepexlegit/parkour/Parkour.java
@@ -0,0 +1,16 @@
+package de.vepexlegit.parkour;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Parkour.MODID, version = Parkour.VERSION)
+public class Parkour {
+    public static final String MODID = "parkour";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    }
+}

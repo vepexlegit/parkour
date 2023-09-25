9755e4afe7b2f9fb238d1fec3ac532f6a1170a85
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-25 11:47:00 +0400

    Update MixinEntityPlayer.java

--- src/main/java/de/vepexlegit/parkour/mixins/MixinEntityPlayer.java
+++ src/main/java/de/vepexlegit/parkour/mixins/MixinEntityPlayer.java
@@ -1,21 +1,21 @@
 package de.vepexlegit.parkour.mixins;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(KeyBinding.class)
+@Mixin(EntityPlayer.class)
 public class MixinEntityPlayer {
 
     private static Minecraft mc = Minecraft.getMinecraft();
 
-    @Inject(method = "isKeyDown", at = @At("RETURN"))
-    public void isKeyDown(final CallbackInfoReturnable<Boolean> cir) {
-        if (mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.theWorld.getCollidingBoundingBoxes((Entity)mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0.0D, -0.5D, 0.0D).expand(-0.001D, 0.0D, -0.001D)).isEmpty()) {
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onUpdate(final CallbackInfo ci) {
+        if (mc.currentScreen == null && mc.theWorld.getCollidingBoundingBoxes((Entity)mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0.0, -0.5, 0.0).expand(0.001, 0.0, 0.001)).isEmpty() && mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.gameSettings.keyBindForward.isKeyDown()) {
             mc.thePlayer.jump();
         }
     }

5f2623baf19eba8497735d1cadf77713d3b54853
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-25 12:05:59 +0400

    Update MixinEntityPlayer.java

--- src/main/java/de/vepexlegit/parkourdebug/mixins/MixinEntityPlayer.java
+++ src/main/java/de/vepexlegit/parkourdebug/mixins/MixinEntityPlayer.java
@@ -3,27 +3,27 @@ package de.vepexlegit.parkourdebug.mixins;
 import de.vepexlegit.parkourdebug.Parkour;
 import de.vepexlegit.parkourdebug.commands.ParkourCommand;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.ClientCommandHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(KeyBinding.class)
+@Mixin(EntityPlayer.class)
 public class MixinEntityPlayer {
-    @Inject(method = "isKeyDown", at = @At("RETURN"))
-    private void startGame(final CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "onUpdate", at = @At("RETURN"))
+    private void startGame(final CallbackInfo ci) {
         ClientCommandHandler.instance.registerCommand(new ParkourCommand());
     }
 
-    public Minecraft mc = Minecraft.getMinecraft();
+    private static Minecraft mc = Minecraft.getMinecraft();
 
-    @Inject(method = "isKeyDown", at = @At("RETURN"))
-    private void isKeyDown(final CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onUpdate(final CallbackInfo ci) {
         if (Parkour.INSTANCE.isEnabled()) {
-            if (mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.theWorld.getCollidingBoundingBoxes((Entity) mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0.0D, -0.5D, 0.0D).expand(-0.001D, 0.0D, -0.001D)).isEmpty()) {
+            if (mc.currentScreen == null && mc.theWorld.getCollidingBoundingBoxes((Entity) mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0.0, -0.5, 0.0).expand(0.001, 0.0, 0.001)).isEmpty() && mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.gameSettings.keyBindForward.isKeyDown()) {
                 mc.thePlayer.jump();
             }
         }
